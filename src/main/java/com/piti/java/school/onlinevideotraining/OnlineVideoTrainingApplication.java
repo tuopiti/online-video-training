e3d50b724b6497ee82eb0a3953fb226c01d0a521
tuopiti <64743303+tuopiti@users.noreply.github.com>
2023-05-31 13:50:34 +0700

    first commit

---
+++ src/main/java/com/piti/java/school/onlinevideotraining/OnlineVideoTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.piti.java.school.onlinevideotraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineVideoTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineVideoTrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/piti/java/school/onlinevideotraining/OnlineVideoTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piti.java.school.onlinevideotraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineVideoTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
